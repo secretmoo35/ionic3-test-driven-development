ccd50cb69b9151d6a3b43684e311a0f6c267fdb0
secretmoo35 <sarayut-moo.th@hotmail.com>
2018-05-14 15:44:30 +0700

    [INITIAL] Project

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,44 @@
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { MyApp } from './app.component';
+import { IonicModule, Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { PlatformMock, StatusBarMock, SplashScreenMock } from '../../test-config/mocks-ionic';
+
+describe('App Component', () => {
+
+  let de: DebugElement;
+  let comp: MyApp;
+  let fixture: ComponentFixture<MyApp>;
+
+  beforeEach(async(() => {
+
+    TestBed.configureTestingModule({
+      declarations: [MyApp],
+      imports: [
+        IonicModule.forRoot(MyApp)
+      ],
+      providers: [
+        { provide: StatusBar, useClass: StatusBarMock },
+        { provide: SplashScreen, useClass: SplashScreenMock },
+        { provide: Platform, useClass: PlatformMock }
+      ]
+    });
+
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MyApp);
+    comp = fixture.componentInstance;
+  });
+
+  it('should be create app component', () => {
+    expect(comp instanceof MyApp).toBe(true);
+  });
+
+  it('should be rootpage is home', () => {
+    expect(comp['rootPage']).toBe('HomePage');
+  });
+
+});
